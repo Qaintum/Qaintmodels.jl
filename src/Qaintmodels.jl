a4f2f34ebb663ce8c72c6ac9a8eb6ed4c904694c
Qunsheng Huang <keefe.huang@tum.de>
2021-02-05 13:35:36 +0100

    Update QaintModels to Qaintmodels

---
+++ src/Qaintmodels.jl
@@ -0,0 +1,15 @@
+module QaintModels
+
+using LinearAlgebra
+using SparseArrays
+using Qaintessent
+
+include("models.jl")
+export
+    qft_circuit,
+    toffoli_circuit,
+    vbe_adder_circuit,
+    qcla_out_adder_circuit,
+    qcla_inplace_adder_circuit
+
+end # module
